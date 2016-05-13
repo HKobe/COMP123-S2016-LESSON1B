94c989eef9dda90f0fc72032b19e16099e498ffd
HKobe <HKobeissi1@hotmail.com>
2016-05-13 10:43:53 -0400

    Initial Commit

---
+++ COMP123- S2016 - Lesson1B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Author: Hassan Kobeissi
+ * Date Created: Friday, 13th, 2016
+ * Date Modified: Friday, 13th, 2016
+ * StudentID: 300847239
+ * Description: This version demonstrates Github and Version Control best practices. We also explore Unit Testing.
+ * Version: 0.1 - initial Version 
+ */
+
+namespace COMP123__S2016___Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
